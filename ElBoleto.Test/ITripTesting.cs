8e68b1d5adf1132d5f53d7b60247466e6db5a5e9
rosejehilan <44560523+rosejehilan@users.noreply.github.com>
2023-07-08 18:15:56 -0500

    Trip unit Testing
    Trip unit Testing

---
+++ ElBoleto.Test/ITripTesting.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ElBoleto.Test
+{
+    public class ITripTesting
+    {
+        //Arrange
+
+
+        //Act
+
+
+        //Assert
+    }
+}
